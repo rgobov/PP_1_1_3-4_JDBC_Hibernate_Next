3793844a21a67080e1041df72334ebc396892243
rgobov <rgobov@gmail.com>
2025-02-07 18:19:19 +0200

    коммитт с методлом удаления таблицы

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,41 +7,10 @@ import org.hibernate.Transaction;
 
 
 public class Main {
-    public static void createUsersTable() {
-        SessionFactory sf = Util.getSessionFactory();
-        try (Session session = sf.openSession()) {
-            // Начинаем транзакцию
-            Transaction transaction = session.beginTransaction();
 
-            try {
-                // Выполняем SQL-запрос
-                session.createSQLQuery("""
-                    CREATE TABLE IF NOT EXISTS users
-                    (
-                        id        INT PRIMARY KEY AUTO_INCREMENT,
-                        name      VARCHAR(45) NOT NULL,
-                        lastname  VARCHAR(45) NOT NULL,
-                        age       TINYINT     NOT NULL
-                    )
-                    """).executeUpdate();
-
-                // Фиксируем транзакцию
-                transaction.commit();
-                System.out.println("Пользовательская таблица создана");
-            } catch (Exception e) {
-                // Откатываем транзакцию в случае ошибки
-                if (transaction != null) {
-                    transaction.rollback();
-                }
-                e.printStackTrace(); // Логируем ошибку
-            }
-        } catch (Exception e) {
-            e.printStackTrace(); // Логируем ошибку при открытии сессии
-        }
-    }
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        createUsersTable();
+
 
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,15 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    SessionFactory sf = Util.getSessionFactory();
     public UserDaoHibernateImpl() {
 
     }
@@ -13,11 +18,63 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void createUsersTable() {
 
+        try (Session session = sf.openSession()) {
+            // Начинаем транзакцию
+            Transaction transaction = session.beginTransaction();
+
+            try {
+                // Выполняем SQL-запрос
+                session.createSQLQuery("""
+                    CREATE TABLE IF NOT EXISTS users
+                    (
+                        id        INT PRIMARY KEY AUTO_INCREMENT,
+                        name      VARCHAR(45) NOT NULL,
+                        lastname  VARCHAR(45) NOT NULL,
+                        age       TINYINT     NOT NULL
+                    )
+                    """).executeUpdate();
+
+                // Фиксируем транзакцию
+                transaction.commit();
+                System.out.println("Пользовательская таблица создана");
+            } catch (Exception e) {
+                // Откатываем транзакцию в случае ошибки
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                e.printStackTrace(); // Логируем ошибку
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // Логируем ошибку при открытии сессии
+        }
     }
 
     @Override
     public void dropUsersTable() {
 
+        try (Session session = sf.openSession()) {
+            // Начинаем транзакцию
+            Transaction transaction = session.beginTransaction();
+
+            try {
+                // Выполняем SQL-запрос
+                session.createSQLQuery("""
+                DROP TABLE IF EXISTS users
+                """).executeUpdate();
+
+                // Фиксируем транзакцию
+                transaction.commit();
+                System.out.println("Пользовательская таблица удалена");
+            } catch (Exception e) {
+                // Откатываем транзакцию в случае ошибки
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                e.printStackTrace(); // Логируем ошибку
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // Логируем ошибку при открытии сессии
+        }
     }
 
     @Override

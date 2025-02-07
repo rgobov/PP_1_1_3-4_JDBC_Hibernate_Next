8191bb790ce2536156bc1e8ffbc70e09f0c618a1
rgobov <rgobov@gmail.com>
2025-02-07 12:47:06 +0200

    first commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,10 +1,44 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
+    private static SessionFactory sessionFactory;
+
+    static {
+        try {
+            // Создание конфигурации Hibernate
+            Configuration configuration = new Configuration();
+            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+            configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/mydb");
+            configuration.setProperty("hibernate.connection.username", "admin");
+            configuration.setProperty("hibernate.connection.password", "root");
+            configuration.setProperty("show_sql", "true");
+
+            // Добавьте ваши сущности
+            configuration.addAnnotatedClass(User.class);
+
+            // Создание SessionFactory
+            sessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        // Закрытие кэшированных соединений
+        getSessionFactory().close();
+    }
     // реализуйте настройку соеденения с БД
     private static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/mydb?user=admin&password=root";
 
@@ -15,5 +49,8 @@ public class Util {
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(CONNECTION_STRING);
     }
+
+
+
 }
 
